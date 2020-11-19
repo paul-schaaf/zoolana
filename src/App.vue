17893dec484dc504ecc9d4cbf18f38b0a3951bcb
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-19 21:47:53 +0100

    add video+audio capture

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <video ref="video" muted />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, onMounted, Ref, ref } from "vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld
+  setup() {
+    const constraints = { audio: true, video: { width: 450, height: 360 } };
+    const video: Ref<null | HTMLVideoElement> = ref(null);
+
+    onMounted(async () => {
+      if (!video.value) {
+        return;
+      }
+      video.value.srcObject = await navigator.mediaDevices.getUserMedia(
+        constraints
+      );
+      video.value.onloadedmetadata = function(e) {
+        if (!video.value) {
+          return;
+        }
+        video.value.play();
+      };
+    });
+
+    return { video };
   }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
