7add291a9c2337cb6c8566ebc58078ffeab9b913
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-12-22 18:19:56 +0100

    add instruction modal

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,19 @@
 .bg-blue-gradient {
   background: linear-gradient(135deg, #0052d4, #6fb1fc);
 }
+
+.bg-gray-gradient {
+  background: linear-gradient(135deg, #0b0b0b, #282828);
+}
+
+.shadow-main {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.transform-middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="h-screen w-screen bg-gray-900 flex justify-center text-white">
+  <div
+    class="h-screen w-screen relative bg-gray-900 flex justify-center text-white"
+  >
     <div class="w-full md:w-1/2 flex flex-col">
       <h1 class="text-4xl text-center mt-64">Zoolana</h1>
       <p class="text-center mt-8">
-        Click <span class="underline cursor-pointer">here</span> for
-        instructions
+        Click
+        <span class="underline cursor-pointer" @click="showInstructions = true"
+          >here</span
+        >
+        for instructions
       </p>
       <div class="flex justify-center mt-9">
         <div
@@ -23,6 +28,41 @@
         Join room
       </button> -->
     </div>
+    <div v-if="showInstructions" class="absolute h-screen w-screen z-50">
+      <div class="relative h-screen w-screen">
+        <div
+          class="transform-middle w-1/3 h-auto bg-gray-gradient rounded-lg shadow-main"
+        >
+          <div class="relative flex flex-col items-center">
+            <div class="w-2/3 flex flex-col items-center text-center mb-12">
+              <p class="mt-12">1. Connect your wallet</p>
+              <p class="mt-3">2. Start or join call</p>
+              <p class="mt-3">
+                2a. If you've clicked 'Start Call', copy the secret room id and
+                send it to your friend
+              </p>
+              <p class="mt-3">
+                2b. If you've clicked 'Join Call', paste the secret room id your
+                friend sent you and click 'Join'
+              </p>
+              <p class="mt-3">
+                3. Do not close the modal. Wait for wallet confirmation
+                requests. There may be up to 20 (tip: use a wallet with only a
+                little sol (in case you don't trust the UI) and turn on auto
+                accept in the first confirmation popup). Accept them all and you
+                will be connected.
+              </p>
+            </div>
+            <div
+              class="absolute top-2 right-4 cursor-pointer select-none text-2xl"
+              @click="showInstructions = false"
+            >
+              X
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +77,7 @@ export default defineComponent({
   setup() {
     const router = useRouter();
     const accountSecret = ref("");
+    const showInstructions = ref(false);
 
     const copyRoomSecret = () => {
       copy(room.roomId.value);
@@ -63,7 +104,7 @@ export default defineComponent({
       router.push({ name: "Room" });
     };
 
-    return { onCreateRoom, onJoinRoom, accountSecret };
+    return { onCreateRoom, onJoinRoom, accountSecret, showInstructions };
   }
 });
 </script>
