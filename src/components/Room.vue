3a367872b3ddb87fae07875680044f53d2dca4a3
Paul <paulsimonschaaf@gmail.com>
2020-12-24 01:37:46 +0100

    room ui (#8)

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,59 +1,83 @@
 <template>
-  <div class="container bg-red-100 flex justify-center">
-    <div class="mt-10 w-2/3 bg-blue-200 flex flex-col items-center">
-      <h1 class="text-4xl">Zoolana</h1>
-      <h2 class="text-xl mt-10 cursor-pointer" @click="copyRoomSecret">
-        Copy Room Secret
-      </h2>
-      <div class="w-4/5 mt-10">
-        <div
-          style="height: 550px"
-          class="flex flex-col items-center bg-red-400 w-full relative"
-        >
-          <video
-            ref="theirVideo"
-            class="w-full h-full bg-red-500"
-            style="object-fit: cover;"
-          ></video>
-          <video
-            ref="myVideo"
-            class="w-48 absolute bg-blue-400 bottom-5 right-5"
-            style="object-fit: cover;"
-            muted
-          ></video>
-        </div>
+  <div class="h-screen w-screen bg-gray-900 flex justify-center">
+    <div class="mt-12 flex flex-col text-white items-center">
+      <h1 class="text-4xl select-none">Zoolana</h1>
+      <div class="relative mt-12 their-video">
+        <video
+          ref="theirVideo"
+          class="w-full h-full bg-gray-800"
+          style="object-fit: cover;"
+        ></video>
+        <video
+          ref="myVideo"
+          class="w-48 absolute bg-gray-700 bottom-5 right-5"
+          style="object-fit: cover;"
+          muted
+        ></video>
       </div>
-      <div class="w-1/3">
-        <div class="mt-10 flex justify-around">
-          <div
-            class="h-20 w-20 bg-gray-400 rounded-full relative cursor-pointer transition-colors"
-            :class="{ 'bg-gray-700': isMuted }"
-            @click="toggleOutgoingMic"
+      <div class="mt-10 w-64 h-20 flex justify-between">
+        <div class="relative little-round-btn" @click="toggleOutgoingMic">
+          <svg
+            v-if="isMuted"
+            alt="unmute"
+            class="absolute top-7 left-7 select-none"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           >
-            <img
-              v-if="isMuted"
-              src="@/assets/icons/mic-off.svg"
-              alt="unmute"
-              class="absolute top-7 left-7"
-            />
-            <img
-              v-else
-              src="@/assets/icons/mic.svg"
-              alt="mute"
-              class="absolute top-7 left-7"
-            />
-          </div>
-          <div
-            class="h-20 w-20 bg-gray-400 rounded-full relative cursor-pointer"
-            @click="onEndCall"
+            <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
+            <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
+            <line x1="12" y1="19" x2="12" y2="23" />
+            <line x1="8" y1="23" x2="16" y2="23" />
+            <line x1="1" y1="1" x2="23" y2="23" stroke="red" />
+          </svg>
+          <svg
+            v-else
+            class="absolute top-7 left-7 select-none"
+            alt="mute"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           >
-            <img
-              src="@/assets/icons/phone-missed.svg"
-              alt="end call"
-              class="absolute"
-              style="top: 1.85rem; left: 1.6rem"
+            <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
+            <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+            <line x1="12" y1="19" x2="12" y2="23" />
+            <line x1="8" y1="23" x2="16" y2="23" />
+          </svg>
+        </div>
+        <div class="relative little-round-btn" @click="onEndCall">
+          <svg
+            alt="end call"
+            class="absolute"
+            style="top: 1.85rem; left: 1.6rem"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="23" y1="1" x2="17" y2="7" />
+            <line x1="17" y1="1" x2="23" y2="7" />
+            <path
+              d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
             />
-          </div>
+          </svg>
         </div>
       </div>
     </div>
@@ -99,3 +123,27 @@ export default defineComponent({
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.their-video {
+  width: 800px;
+  height: 500px;
+
+  @media (min-width: 300px) {
+    width: 300px;
+  }
+
+  @media (min-width: 400px) {
+    width: 400px;
+  }
+
+  @media (min-width: 600px) {
+    width: 600px;
+  }
+
+  @media (min-height: 850px) and (min-width: 1000px) {
+    width: 1000px;
+    height: 600px;
+  }
+}
+</style>
